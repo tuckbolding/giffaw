a5f303fff86127255ebb3f37c9d80e24ca0200f1
tuckbolding <tuckbolding@gmail.com>
2017-10-03 18:42:31 -0700

    integrated giphy api

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,36 @@
 $(document).on("ready", function(){
 
+
+
+$("form").on("submit", function(e) {
+    e.preventDefault();
+
+var giphy_api = "http://api.giphy.com/v1/gifs/search?";
+
+		// function getAndRenderGifs() {
+		  $.ajax({
+		    method: "GET",
+		    url: giphy_api,
+		    data: $("form").serialize(),
+		    success: onSuccess,
+		    error: onError
+		  });
+		// }
+
+
+		function onSuccess(json) {
+			for (var i=0; i<json.data.length; i++) {
+			$(".gif-gallery").append('<img src="'+json.data[i].images.fixed_width_small.url+'">');
+		}}
+
+		function onError(xhr, status, errorThrown) {
+			alert("Sorry, there was a problem!");
+			console.log("Error: " + errorThrown);
+			console.log("Status: " + status);
+			console.dir(xhr);
+		}
+// getAndRenderGifs();
 });
+});	
+		
+
